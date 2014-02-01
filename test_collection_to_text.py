0becd7285aec0f6790379129f8dd6c66a8d2a774
Romain Deveaud <romain.deveaud@gmail.com>
2014-02-01 19:58:35 +0000

    license

--- test_collection_to_text.py
+++ test_collection_to_text.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python
+"""Copyright (C) 2014 Romain Deveaud <romain.deveaud@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 
 import sys
 import time
@@ -17,9 +33,11 @@ from trec import *
 from utils import *
 
  
-# width defines bar width
-# percent defines current percentage
 def progress(width, percent):
+    """
+    `width` defines the width of the progressbar
+    `percent` defines the current progress percentage 
+    """
     marks = math.floor(width * (percent / 100.0))
     spaces = math.floor(width - marks)
      
@@ -61,23 +79,32 @@ if __name__ == "__main__":
 
   print("Parsing all runs from ("+ ', '.join(options['folders']) +"), collection index: "+options['coll'])
 
+  # get a list of docnos from the qrel file.
   documents = unique(get_docs_from_qrels(options['qrels']))
+
   print("qrels done ("+str(len(documents))+" documents).")
 
+  # get the list of all docnos for all the TREC runs contained in the
+  # supplied directory.
   for folder in options['folders']:
     for file in glob.glob(folder+'/*'):
       documents += get_docs_from_run(file,100)
 
+  # only keep the unique docnos.
   documents = unique(documents)
 
   print("Runs parsed ! "+ str(len(documents)) + " documents.")
   print("Now fetching text...")
 
+
+  # create the output XML file that will contain the text of all the documents
+  # identified by the docnos in `documents`.
   output = open(options['out'],'w')
 
   output.write("<?xml version='1.0' encoding='UTF-8' standalone='no' ?>\n")
   output.write("<documents>\n")
 
+  # initialisation of the progressbar.
   widgets = ["Finished ", Counter(), " documents so far. ",
               Bar(), ' ',
               Percentage(), ' ',
@@ -87,17 +114,19 @@ if __name__ == "__main__":
 
   i = 0
   for d in documents:
+    # get the internal Indri id associated with the current docno `d`, and
+    # get the text of this document.
     indri_id = subprocess.Popen(["dumpindex", options['coll'], "di" ,"docno", d],stdout=subprocess.PIPE).communicate()[0].strip()
     doc      = subprocess.Popen(["dumpindex", options['coll'], "dt", indri_id],stdout=subprocess.PIPE).communicate()[0].strip()
 
     output.write("<document id=\""+d+"\">")
 
+    # remove the HTML from the text, then write in the XML file.
     cleaner = Cleaner(kill_tags=['script','SCRIPT','style'],page_structure=False)
     text = lxml.html.document_fromstring(cleaner.clean_html(doc))
     output.write(re.sub(' +', ' ', re.sub(r"[\n\r\t\W]",' ',text.text_content()))+"\n")
 
     output.write("</document>\n")
-#progress(50, (i + 1)/len(documents)) 
     i+=1
     pbar.update(i)
 
